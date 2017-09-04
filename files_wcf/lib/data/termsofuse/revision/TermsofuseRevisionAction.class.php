a521e6e2085f4fc10d1605e35c205e763fc0c36f
Tim Düsterhus <tim@bastelstu.be>
2017-09-04 21:12:52 +0200

    Add database objects

---
+++ files_wcf/lib/data/termsofuse/revision/TermsofuseRevisionAction.class.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * Copyright (c) 2017, Tim Düsterhus
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace wcf\data\termsofuse\revision;
+
+/**
+ * Executes terms of use revision-related actions.
+ */
+class TermsofuseRevisionAction extends \wcf\data\AbstractDatabaseObjectAction {
+	/**
+	 * @inheritDoc
+	 */
+	protected $permissionsDelete = [ ];
+
+	/**
+	 * @inheritDoc
+	 */
+	protected $permissionsUpdate = [ ];
+}
