78f160591fd33231c46dbc148367b39c460bd452
Tim Düsterhus <tim@bastelstu.be>
2017-09-04 21:07:17 +0200

    Add sql/0002-wcf1_termsofuse_revision.sql

---
+++ sql/0002-wcf1_termsofuse_revision.sql
@@ -0,0 +1,14 @@
+CREATE TABLE wcf1_termsofuse_revision ( revisionID INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY
+                                      , enabledAt  INT(10) DEFAULT NULL
+                                      );
+
+CREATE TABLE wcf1_termsofuse_revision_content ( contentID  INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY
+                                              , revisionID INT(10) NOT NULL
+                                              , languageID INT(10) NOT NULL
+                                              , content MEDIUMTEXT
+                                              , UNIQUE KEY(revisionID, languageID)
+                                              );
+
+ALTER TABLE wcf1_termsofuse_revision_content ADD FOREIGN KEY (revisionID) REFERENCES wcf1_termsofuse_revision (revisionID) ON DELETE CASCADE;
+ALTER TABLE wcf1_termsofuse_revision_content ADD FOREIGN KEY (languageID) REFERENCES wcf1_language (languageID) ON DELETE CASCADE;
+ALTER TABLE wcf1_user ADD FOREIGN KEY (termsOfUseRevision) REFERENCES wcf1_termsofuse_revision (revisionID) ON DELETE SET NULL;
