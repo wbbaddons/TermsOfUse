68325a89a075db704f5b6f0ed92f2e12c3cf012f
Tim Düsterhus <tim@bastelstu.be>
2017-09-16 17:41:58 +0200

    Force re-acceptance of Terms Of Use

---
+++ files_wcf/lib/system/event/listener/ControllerTermsOfUseListener.class.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * Copyright (c) 2017, Tim Düsterhus
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace wcf\system\event\listener;
+
+use \wcf\data\termsofuse\revision\TermsofuseRevision;
+use \wcf\system\exception\AJAXException;
+use \wcf\system\request\LinkHandler;
+use \wcf\system\request\RequestHandler;
+use \wcf\system\WCF;
+use \wcf\util\HeaderUtil;
+
+/**
+ * Forces re-acceptance of newer terms of use.
+ */
+class ControllerTermsOfUseListener implements IParameterizedEventListener {
+	/**
+	 * @inheritDoc
+	 */
+	public function execute($eventObj, $className, $eventName, array &$parameters) {
+		if (!WCF::getUser()->userID) return;
+		$active = TermsofuseRevision::getActiveRevision();
+		if (WCF::getUser()->termsOfUseRevision === $active->revisionID) return;
+		if (RequestHandler::getInstance()->getActiveRequest()->isAvailableDuringOfflineMode()) return;
+		
+		if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && ($_SERVER['HTTP_X_REQUESTED_WITH'] === 'XMLHttpRequest')) {
+			throw new AJAXException(WCF::getLanguage()->getDynamicVariable('wcf.ajax.error.permissionDenied'), AJAXException::INSUFFICIENT_PERMISSIONS);
+		}
+		else {
+			HeaderUtil::redirect(LinkHandler::getInstance()->getLink('TermsOfUse'));
+			exit;
+		}
+	}
+}
